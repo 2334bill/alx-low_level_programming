f3de3da189646a567326c826aa1ce2579862a468
2334bill <billolesh777@gmail.com>
2023-07-07 01:26:40 -0700

    this is for the power of a digit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*_pow_recursion - prints the power of a number
+*@y:one number
+*@x: second number
+*Return: the power of a number
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0)
+{
+return (1);
+}
+else
+{
+return (x * _pow_recursion(x,y - 1));
+}
+}
+ 
