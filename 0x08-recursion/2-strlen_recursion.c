28bbed7d0c1c653c1375349df634222cae5f6f13
2334bill <billolesh777@gmail.com>
2023-07-07 00:21:48 -0700

    this is for reversing a sting

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_strlen_recursion - prints the lenght of a string
+*@s: the string
+*@i: for the length
+*Return: nothing
+*/
+int _strlen_recursion(char *s)
+{
+int i;
+i = 0;
+if(*s != '\0')
+{
+return i;
+}
+i++;
+}
+
