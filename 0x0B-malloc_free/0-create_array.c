e9e25c6e6c328cb081669a7777642fceaa9c7358
2334bill <billolesh777@gmail.com>
2023-07-12 22:08:53 -0700

    this is for initiliazing the contents of the array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+*create_array - creates an array
+*@size: soze of the array
+*@c: the contents of the array
+*Return:nothing
+*/
+char *create_array(unsigned int size, char c)
+{
+char *array = (char *)malloc(size * sizeof(char));
+if (array != NULL && size > 0)
+{
+unsigned int i;
+for (i = 0; i < size; i++)
+{
+array[i] = c;
+}
+}
+else
+{
+return (NULL);
+}
+return (array);
+}
