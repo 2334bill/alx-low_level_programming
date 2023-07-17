8385c9d1971159558ed28af5f77d740bde06c263
2334bill <billolesh777@gmail.com>
2023-07-17 13:08:29 -0700

    this is for struct 1

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+this is for structes and type def
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef HUN_H
+#define HUN_H
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*struct dog - details of the dog
+*@name: name
+*@age: age of the dog
+*@owner: owner
+*Return: nothing
+*/
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+my_dog;
+#endif
