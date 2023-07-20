5d9d36f785e40395eb6e5b250b22f23d66b0e942
2334bill <billolesh777@gmail.com>
2023-07-20 14:18:22 -0700

    this is a function that prints strings, followed by a new line

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,6 @@
 #ifndef FUN_H
 #define FUN_H
-
+void print_strings(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 
