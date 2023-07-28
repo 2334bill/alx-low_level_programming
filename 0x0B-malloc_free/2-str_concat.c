d4c009ab36b34a7a92f2e0179256b8a2ae52eb3e
2334bill <billolesh777@gmail.com>
2023-07-28 00:42:16 -0700

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,47 +9,46 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-  int i;
-  int k;
-  int j;
-  int l;
-  char *s;
-  j = 0;
-  i = 0;
-  k = 0;
-  l = 0;
-  if (s1 == NULL)
-    {
-      s1 = "";
-    }
-  if (s2 == NULL)
-    {
-      s2 = "";
-    }
-  while (s1[i])
-    {
-      i++;
-    }
-  while (s2[j])
-    {
-      j++;
-    }
-  l = i + j;
-  s = malloc((sizeof(char) * l) + 1);
-  if (s == NULL)
-    {
-      return (NULL);
-    }
-  while (k < l)
-    {
-      if (k < i)
-	{
-	  s[k] = s1[k];
-	}
-      else
-	{
-	  s[k] = s2[k - i];
-     
+int i;
+int k;
+int j;
+int l;
+char *s;
+j = 0;
+i = 0;
+k = 0;
+l = 0;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+while (s1[i])
+{
+i++;
+}
+while (s2[j])
+{
+j++;
+}
+l = i + j;
+s = malloc((sizeof(char) * l) + 1);
+if (s == NULL)
+{
+return (NULL);
+}
+while (k < l)
+{
+if (k < i)
+{
+s[k] = s1[k];
+}
+else
+{
+s[k] = s2[k - i];
 	}
       k++;
 }
