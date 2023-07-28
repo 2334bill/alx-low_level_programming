a7f2af551f279ee570c4d357f4aa6018819afaad
2334bill <billolesh777@gmail.com>
2023-07-28 00:33:19 -0700

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,22 +9,50 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-char *array;
-int i;
-int len1;
-int len2;
-i = 0;
-len1 = strlen(s1);
-len2 = strlen(s2);
-array = malloc((len1 + len2 + 2) * sizeof(char));
-for (i = 0; i < len1; i++)
-{
-array[i] = s1[i];
+  int i;
+  int k;
+  int j;
+  int l;
+  char *s;
+  j = 0;
+  i = 0;
+  k = 0;
+  l = 0;
+  if (s1 == NULL)
+    {
+      s1 = "";
+    }
+  if (s2 == NULL)
+    {
+      s2 = "";
+    }
+  while (s1[i])
+    {
+      i++;
+    }
+  while (s2[j])
+    {
+      j++;
+    }
+  l = i + j;
+  s = malloc((sizeof(char) * l) + 1);
+  if (s == NULL)
+    {
+      return (NULL);
+    }
+  while (k < l)
+    {
+      if (k < i)
+	{
+	  s[k] = s1[k];
+	}
+      else
+	{
+	  s[k] = s2[k - i];
+     
+	}
+      k++;
 }
-for (i = 0; i < len2; i++)
-{
-array[i + len1] = s2[i];
-}
-array[len1 + len2] = '\0';
-return (array);
+s[k] = '\0';
+return (s);
 }
