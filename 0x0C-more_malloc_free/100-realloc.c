29a0c71c54ecc61d8965c454b67b7aee9ebb2e83
2334bill <billolesh777@gmail.com>
2023-07-29 12:14:23 -0700

    fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,45 +1,50 @@
 #include <stdlib.h>
+#include "main.h"
+
 /**
-* _realloc - Reallocates a memory block using malloc and free.
-*
-* @ptr: Pointer to the memory previously allocated with malloc.
-* @old_size: Size, in bytes, of the allocated space for ptr.
-* @new_size: New size, in bytes, of the new memory block.
-*
-* Return: Pointer to the reallocated memory block, or NULL on failure or if new_size is 0.
-*/
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previsouly allocated by malloc
+ * @old_size: size of the allocated memory for ptr
+ * @new_size: new size of the new memory block
+ *
+ * Return: pointer to the newly allocated memory block
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-void *new_ptr;
-char *src;
-char *dest;
-unsigned int i;
-unsigned int copy_size;
-if (new_size == 0 && ptr != NULL)
-{
-free(ptr);
-return (NULL);
-}
-if (new_size == old_size)
-{
-return (ptr);
-}
-new_ptr = malloc(new_size);
-if (new_ptr == NULL)
-{
-free(ptr);
-return (NULL);
-}
-if (ptr != NULL)
-{
-copy_size = old_size < new_size ? old_size : new_size;
-src = ptr;
-dest = new_ptr;
-for (i = 0; i < copy_size; i++)
-{
-dest[i] = src[i];
-}
-free(ptr);
-}
-return (new_ptr);
+	char *ptr1;
+	char *old_ptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (!ptr)
+		return (malloc(new_size));
+
+	ptr1 = malloc(new_size);
+	if (!ptr1)
+		return (NULL);
+
+	old_ptr = ptr;
+
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	free(ptr);
+	return (ptr1);
 }
