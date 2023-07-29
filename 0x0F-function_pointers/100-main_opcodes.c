ed42b02c318d7c34e240dd06ef0e0aa60c5c009c
2334bill <billolesh777@gmail.com>
2023-07-29 11:54:49 -0700

    fix

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,43 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
-* main - prints
-* @argc: arguments count
-* @argv: arguments themselves
-* Return: 0
-*/
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-int bytes;
-int i;
-unsigned char *arr;
-if (argc != 2)
-{
-printf("Error\n");
-exit(1);
-}
-bytes = atoi(argv[1]);
-if (bytes < 0)
-{
-printf("Error\n");
-exit(2);
-}
-arr = malloc(bytes);
-if (arr == NULL)
-{
-printf("Error\n");
-exit(3);
-}
-for (i = 0; i < bytes; i++)
-{
-arr[i] = ((unsigned char *)&main)[i];
-if (i == bytes - 1)
-{
-printf("%02x\n", arr[i]);
-break;
-}
-printf("%02x", arr[i]);
-}
-free(arr);
-return (0);
+	int bytes, i;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
 }
