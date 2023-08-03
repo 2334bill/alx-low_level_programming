43daf1291889652760c2343d62cfeb5dc9870ec9
2334bill <susanarwa7@gmail.com>
2023-08-03 12:00:56 -0700

    this is for conversion of bit to integer

---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+ this is for the alx project which invloves various bit manipulation task  Done by Bill
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef HUN_N
+#define HUN_N
+
+
+unsigned int binary_to_uint(const char *b);
+
+
+
+
+#endif
