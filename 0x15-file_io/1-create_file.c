c5137f5d2291d0d4da0773735a83544a48c03945
2334bill <billolesh77@gmail.com>
2023-10-06 23:30:30 -0700

    create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,31 +10,29 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-  ssize_t w;
-  char *buffer;
-  int outsize;
-  buffer = (char *)malloc(strlen(text_content) + 1);
-  if (buffer == NULL)
-    {
-      return (-1);
-    }
- 
-  outsize = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-  if (outsize == -1)
-    {
-      free(buffer);
-      return (-1);
-    }
-  strcpy(buffer, text_content);
-  w = write(outsize,buffer,strlen(text_content));
-  if (w == -1)
-    {
-      close(outsize);
-      free(buffer);
-      return (-1);
-    }
-  close(outsize);
-  free(buffer);
-  
-  return (1);
+ssize_t w;
+char *buffer;
+int outsize;
+buffer = (char *)malloc(strlen(text_content) + 1);
+if (buffer == NULL)
+{
+return (-1);
+}
+outsize = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+if (outsize == -1)
+{
+free(buffer);
+return (-1);
+}
+strcpy(buffer, text_content);
+w = write(outsize,buffer,strlen(text_content));
+if (w == -1)
+{
+close(outsize);
+free(buffer);
+return (-1);
+}
+close(outsize);
+free(buffer);
+return (1);
 }
